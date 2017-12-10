5af1b64d96d898342d86672b0847d79844354c5e
AngryGrizzlyBear <evanmlongwood@gmail.com>
2017-12-10 15:22:21 -0800

    Can finally add and delete handles.

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
     
 class ProjectItem extends Component {
-    deleteProject(){
-        console.log('test')
+    deleteProject(id){
+        this.props.onDelete(id);
     }
+
     render() {
         return ( 
         <li className = "Project" >
-         <strong> {this.props.project.title}</strong>: - {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this)}>X</a>
+        
+         <strong> {this.props.project.title}</strong>: - {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
         </li>
         );
     }
