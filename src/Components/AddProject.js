b2e090c23f6ed0fe77490becca5358dfec868457
AngryGrizzlyBear <evanmlongwood@gmail.com>
2017-12-10 00:00:29 -0800

    Submit button works! Output is being console.logged. You can check for yourself.

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 
 
 class AddProject extends Component {
+    constructor (){
+        super();
+        this.state = {
+            newProject:{}
+        }
+    }
+    
     static defaultProps = {
         categories: ['Web Design', 'Web Development', 'Mobile Development']
     }
     
+handleSubmit(e){
+    if(this.refs.title.value === ''){
+        alert('Title is required');
+    } else {
+        this.setState({newProject: {
+           title: this.refs.title.value,
+           category: this.refs.category.value
+        }}, function(){
+            // console.log(this.state);
+            this.props.addProject(this.state.newProject);
+        });
+    }
+    e.preventDefault();
+}
+
     render() {
         let categoryOptions = this.props.categories.map(category => {
             return <option key={category} value="category">{category}</option>
-        })
+        });
         return ( 
         <div>
             <h3>Add Project</h3>
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 <div>
                     <label>Title</label><br />
                     <input type="text" ref="title" />
@@ -23,9 +45,9 @@ class AddProject extends Component {
                     <select ref="category">
                         {categoryOptions}
                     </select>
+                    <input type="submit" value="Submit" />
                 </div>
             </form>
-
         </div>
         );
     }
